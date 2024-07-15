c4c7f804bfb66e9b0d4987ba3b26845a585f5b86
deyanovBoris <133798115+deyanovBoris@users.noreply.github.com>
2024-07-15 17:20:40 +0300

    Adjustments to internationalization + implement scheduling of removal of rejected tutor applications;

--- src/main/java/bg/softuni/tutorme/repositories/TutorApplicationRepository.java
+++ src/main/java/bg/softuni/tutorme/repositories/TutorApplicationRepository.java
@@ -5,10 +5,13 @@ import bg.softuni.tutorme.entities.enums.ApplicationStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TutorApplicationRepository extends JpaRepository<TutorApplication, Long> {
     boolean existsByApplicantUsernameAndIsCompletedFalse(String username);
 
     boolean existsByApplicantUsernameAndApplicationStatus(String username, ApplicationStatusEnum applicationStatusEnum);
 
+    List<TutorApplication> findAllByIsCompletedTrueAndApplicationStatus(ApplicationStatusEnum applicationStatusEnum);
 }
---
+++ src/main/java/bg/softuni/tutorme/service/ScheduledTasks.java
@@ -0,0 +1,41 @@
+package bg.softuni.tutorme.service;
+
+import bg.softuni.tutorme.entities.TutorApplication;
+import bg.softuni.tutorme.entities.enums.ApplicationStatusEnum;
+import bg.softuni.tutorme.repositories.TutorApplicationRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+
+@Component
+public class ScheduledTasks {
+
+    private final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private final TutorApplicationRepository tutorApplicationRepository;
+
+    public ScheduledTasks(TutorApplicationRepository tutorApplicationRepository) {
+        this.tutorApplicationRepository = tutorApplicationRepository;
+    }
+
+    @Scheduled(cron = "0 */30 * * * *")
+    public void clearUnapprovedTutorRequests(){
+        logger.info("Performing cleaning of unapproved tutor requests");
+
+        List<TutorApplication> rejectedApplications = this.tutorApplicationRepository
+                .findAllByIsCompletedTrueAndApplicationStatus(ApplicationStatusEnum.REJECTED);
+
+        String ids = rejectedApplications
+                .stream()
+                .map(app -> app.getId())
+                .collect(Collectors.toList())
+                .toString();
+        logger.info("Deleting request ids: " + ids);
+
+        this.tutorApplicationRepository.deleteAll(rejectedApplications);
+    }
+}
