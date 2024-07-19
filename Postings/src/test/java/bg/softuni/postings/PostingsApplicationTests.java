d707fb4936174554e243d0d334153f72d692856f
deyanovBoris <133798115+deyanovBoris@users.noreply.github.com>
2024-07-19 11:29:03 +0300

    Add new module that will be responsible for fulfilling the rest service requirement

---
+++ Postings/src/test/java/bg/softuni/postings/PostingsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.postings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
