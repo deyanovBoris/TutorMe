a631803e94d5265ef27a0425523e0184b0a61350
deyanovBoris <133798115+deyanovBoris@users.noreply.github.com>
2024-07-23 13:33:48 +0300

    Remove add-post.html and some unused imports

--- src/main/java/bg/softuni/tutorme/service/CourseService.java
+++ src/main/java/bg/softuni/tutorme/service/CourseService.java
@@ -1,15 +1,12 @@
 package bg.softuni.tutorme.service;
 
-import bg.softuni.tutorme.entities.Course;
 import bg.softuni.tutorme.entities.dtos.courses.CourseAddDTO;
 import bg.softuni.tutorme.entities.dtos.courses.CourseInfoDTO;
 import bg.softuni.tutorme.entities.dtos.courses.CourseShortInfoDTO;
 import bg.softuni.tutorme.service.exceptions.UserNotFoundException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CourseService {
     List<CourseShortInfoDTO> getAllCoursesBySubjectId(long subjectId);
